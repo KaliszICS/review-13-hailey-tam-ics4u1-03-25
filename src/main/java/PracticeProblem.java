d5497e18c8e2b729b4a2240de6d519720c8f19eb
hailey-tam-ics4u1-03-25 <hailey.tam@student.tdsb.on.ca>
2025-02-27 00:05:42 +0000

    Completed Code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,39 @@
-import java.util.*;
 
+import java.util.HashMap;
+import java.util.ArrayList;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-}
+	public static void swap(ArrayList<String> arr, int num1, int num2) {
+		String temp1 = arr.get(num1);
+		String temp2 = arr.get(num2);
+		arr.set(num1,temp2);
+		arr.set(num2, temp1);
+	}
+
+	public static ArrayList<Double> createArrayList(double[] arr){
+		ArrayList<Double> arrNew = new ArrayList<Double>();
+		for (int i = 0; i < arr.length; i ++){
+			arrNew.add(arr[i]);
+		}
+		return arrNew;
+	}
+
+	public static HashMap<String, Integer> combineParallelArrays(String[] names, int[] ages){
+		HashMap<String, Integer> map = new HashMap<String,Integer>();
+		for (int i = 0; i < names.length; i++){
+			map.put(names[i], ages[i]);
+		}
+		return map;
+	}
+
+	public static void increaseAge(HashMap<String,Integer> age, String name){
+		Integer currentAge = age.get(name);
+		Integer newAge = currentAge + 1;
+		age.put(name, newAge);
+		}
+
+	}
